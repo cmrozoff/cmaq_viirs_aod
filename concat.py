4253c0bde27af19e5483cd13a13f6fc90c2ddde8
Christopher Rozoff <cmrozoff@gmail.com>
2025-08-06 12:04:59 -0600

    added file info

--- concat.py
+++ concat.py
@@ -8,3 +8,10 @@ start_date_arg = "20200815_12"
 input_dir = f"/glade/work/rozoff/fire/viirs/viirs_on_model_grid/{start_date_arg}"
 output_dir = f"/glade/work/rozoff/fire/viirs/model_structure/{start_date_arg}"
 os.makedirs(output_dir, exist_ok=True)
+#
+# Forecast hours to loop over
+forecast_hours = [f"f{str(i).zfill(3)}" for i in range(73)]
+#
+# Regex to capture forecast hour
+pattern = re.compile(r"viirs_on_model_(f\d{3})_AER*\.nc$")
+print(forecast_hours)
