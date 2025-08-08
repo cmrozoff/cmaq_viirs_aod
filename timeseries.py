0d2c5868206a6906bd2664ccf32ccb2e741c4611
Christopher Rozoff <cmrozoff@gmail.com>
2025-08-08 12:48:43 -0600

    added all the looping

--- timeseries.py
+++ timeseries.py
@@ -1,4 +1,9 @@
+#
+# USER CONFIGURATION
+input_dir = "/glade/work/rozoff/fire/viirs/model_structure/"
+#
 from datetime import datetime, timedelta
+from sys import exit
 #
 # Start and end dates
 start_date = datetime(2020, 8, 18)
@@ -10,7 +15,12 @@ print("Reading in AOD data")
 aod_mod = None
 aod_obs = None
 #
+# Forecast hours to loop over
+forecast_hours = [f"f{str(i).zfill(3)}" for i in range(73)]
+#
 while current_date <= end_date:
     #
     print(current_date)
+    for fhr in forecast_hours:
+        print(f"Processing forecast hour {fhr} ...")
     current_date += timedelta(days=1)
